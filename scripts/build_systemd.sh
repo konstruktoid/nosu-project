bd542070d04c99213b585cdcb14d101404f4951b
Thomas Sjögren <konstruktoid@users.noreply.github.com>
2025-01-06 23:49:51 +0000

    install latest release
    Signed-off-by: Thomas Sjögren <konstruktoid@users.noreply.github.com>

--- scripts/build_systemd.sh
+++ scripts/build_systemd.sh
@@ -2,12 +2,13 @@
 
 set -eux
 
-RELEASE="256.4"
-URI="https://github.com/systemd/systemd/archive/refs/tags/v${RELEASE}.tar.gz"
-BUILD_DIR="/tmp/build-systemd-v${RELEASE}"
+RELEASE=$(curl -fsSL https://api.github.com/repos/systemd/systemd/releases/latest | jq -r '.["tag_name"]')
+NUM_RELEASE="$(echo -n "${RELEASE}" | cut -c 2-)"
+URI="https://github.com/systemd/systemd/archive/refs/tags/${RELEASE}.tar.gz"
+BUILD_DIR="/tmp/build-systemd-${RELEASE}"
 INSTALLED_RELEASE="$(systemctl --version | head -n 1 | awk '{print $2}')"
 
-if [ "$(echo -n "${INSTALLED_RELEASE}" | cut -c -3)" -ge "$(echo -n "${RELEASE}" | cut -c -3)" ]; then
+if [ "$(echo -n "${INSTALLED_RELEASE}" | cut -c -3)" -ge "$(echo -n "${RELEASE}" | cut -c 2-4)" ]; then
   echo "Installed systemd is already at version 256 or later."
   if echo -n "${INSTALLED_RELEASE}" | grep -q "rc"; then
     if [ "$(echo -n "${RELEASE}" | wc -c)" -gt "3" ]; then
@@ -24,10 +25,12 @@ Installed systemd version is ${INSTALLED_RELEASE}.
 Building systemd version ${RELEASE}.
 "
 
+sudo sed -i s/'^Types: deb$/Types: deb deb-src/g' /etc/apt/sources.list.d/ubuntu.sources
+
 sudo apt-get update
 sudo apt-get --assume-yes upgrade
 sudo apt-get --assume-yes build-dep systemd
-sudo apt-get --assume-yes install python3-pip python3-venv
+sudo apt-get --assume-yes install python3-pip python3-venv --no-install-recommends
 
 python3 -m venv "${BUILD_DIR}"
 
@@ -48,8 +51,8 @@ fi
 
 cd "${BUILD_DIR}"
 wget --no-clobber "${URI}"
-tar -xzvf "v${RELEASE}.tar.gz"
-cd "systemd-${RELEASE}"
+tar -xzvf "${RELEASE}.tar.gz"
+cd "systemd-${NUM_RELEASE}"
 
 if [ -d "/tmp/user" ]; then
   sudo chmod 0755 /tmp/user/ # https://github.com/systemd/systemd/issues/33006
